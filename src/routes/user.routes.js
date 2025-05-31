df1cbad73f3c0123337aa9c35ce6d359f48d2bdf
priyanshu-upload <priyanshupal11352@gmail.com>
2025-05-31 12:26:17 +0530

    refactor user.routes.js to include user-related routes.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from 'express';
-import { loginUser, registerUser, logoutUser, refreshAccessToken } from '../controllers/user.controller.js';
+import {
+    loginUser,
+    registerUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccoutDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -24,5 +36,26 @@ router.route('/login').post(loginUser);
 router.route('/logout').post(verifyJWT, logoutUser);
 router.route('/refresh-token').post(refreshAccessToken)
 
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+
+router.route('/current-user').get(verifyJWT, getCurrentUser);
+
+router.route('/update-account').patch(verifyJWT, updateAccoutDetails);
+
+router.route('/avatar').patch(
+    verifyJWT,
+    upload.single('avatar'), // 'avatar' is the field name for the image upload
+    updateUserAvatar
+);
+
+router.route('/cover-image').patch(
+    verifyJWT,
+    upload.single('coverImage'), // 'coverImage' is the field name for the image upload
+    updateUserCoverImage
+);
+
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile); // Get user by username
+
+router.route('/history').get(verifyJWT, getWatchHistory)
 
 export default router;
